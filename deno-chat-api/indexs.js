816de66b608c840a608d211324edccbe745ae7a0
david <innes381@gmail.com>
2022-06-21 16:55:55 +0200

    Changed text to standing mans

--- deno-chat-api/indexs.js
+++ deno-chat-api/indexs.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello Worlds!", {
+  const response = new Response("Standing Mans!", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
