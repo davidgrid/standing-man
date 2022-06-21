6363fe062ce4adff8a6119f38a66f8ba3a3370a8
david <innes381@gmail.com>
2022-06-21 16:58:10 +0200

    standing mans 2

--- deno-chat-api/indexs.js
+++ deno-chat-api/indexs.js
@@ -1,19 +1,6 @@
-/** @jsx h */
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import { h } from "https://esm.sh/preact@10.5.15";
-import { renderToString } from "https://esm.sh/preact-render-to-string@5.1.19?deps=preact@10.5.15";
-
-function handler(_req: Request): Response {
-  const page = (
-    <div>
-      <h1>Current time</h1>
-      <p>{new Date().toLocaleString()}</p>
-    </div>
-  );
-  const html = renderToString(page);
-  return new Response(html, {
-    headers: { "content-type": "text/html; charset=utf-8" },
+addEventListener("fetch", (event) => {
+  const response = new Response("Standing Mans 2!", {
+    headers: { "content-type": "text/plain" },
   });
-}
-
-serve(handler);
+  event.respondWith(response);
+});
