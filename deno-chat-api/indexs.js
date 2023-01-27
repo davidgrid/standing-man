5407b93bc5fd00cb1716f1e53fdfe385d564ebbe
davidgrid <56825390+davidgrid@users.noreply.github.com>
2023-01-27 16:38:00 +0700

    Update indexs.js

--- deno-chat-api/indexs.js
+++ deno-chat-api/indexs.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Standing Man coming soon!", {
+  const response = new Response("Standing Man coming RYan!", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
